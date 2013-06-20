8ec9ab9fbb56ef8e8a319ec1d8d4205175e5f7c4
Masih Hajiarabderkani <masih.h.derkani@st-andrews.ac.uk>
2013-06-20 11:49:21 +0100

    Added Slow test category

---
+++ src/test/java/uk/ac/standrews/cs/test/category/Slow.java
@@ -0,0 +1,16 @@
+package uk.ac.standrews.cs.test.category;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.experimental.categories.Category;
+
+/**
+ * Categorises slow running JUnit tests.
+ * This interface is typically used as the value of a JUnit {@link Category} annotations.
+ *
+ * @author Masih Hajiarabderkani (mh638@st-andrews.ac.uk)
+ * @see Category
+ * @see Categories
+ */
+public interface Slow {
+
+}
